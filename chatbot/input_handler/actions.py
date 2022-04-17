3d2d1de685f0d4abcf8a6b372f374a57bbcac7ff
CATHERINE QI <catherineq5986@gmail.com>
2022-04-17 02:25:37 -0400

    Create actions.py

---
+++ chatbot/input_handler/actions.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+from func_timeout import func_timeout, FunctionTimedOut
+import traceback
+
+class Action(ABC):
+    @staticmethod
+    @abstractmethod
+    def run(conv_list, params):
+        """
+        This is a static method for an abstract class. This method should run the corresponding action.
+        Args:
+            conv_list(list): List of util.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+            params(dict): A dict containing some mandatory and optional parameters.
+        """
+        pass
+
+class RetrievalAction(Action):
+    @staticmethod
+    def run(conv_list, params):
+        """
+        
+        Args:
+            conv_list(list): List of util.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+            params(dict): A dict containing some parameters. The parameter 'retrieval' is required, which should be the
+            retrieval model object.
+        Returns:
+            
+        """
+        return params['actions']['retrieval'].get_results(conv_list)
+
+class ConferenceAction(Action):
+    @staticmethod
+    def run(conv_list, params):
+        """
+
+        Args:
+            conv_list(list): List of util.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+            params(dict): A dict containing some parameters. The parameter 'retrieval' is required, which should be the
+            retrieval model object.
+        Returns:
+
+        """
+        return params['actions']['conference'].get_results(conv_list)
+
+class QuestionAction(Action):
+    @staticmethod
+    def run(conv_list, params):
+        """
+
+        Args:
+            conv_list(list): List of util.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+            params(dict): A dict containing some parameters. The parameter 'retrieval' is required, which should be the
+            retrieval model object.
+        Returns:
+
+        """
+        return params['actions']['question'].get_results(conv_list)
