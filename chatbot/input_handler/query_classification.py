4b424194da8b356061787d4cc55666b025cabefe
CATHERINE QI <catherineq5986@gmail.com>
2022-04-16 22:26:26 -0400

    Update query_classification.py
    Added acceptance/rejection intent. Code refactoring and more documentation also added.

--- chatbot/input_handler/query_classification.py
+++ chatbot/input_handler/query_classification.py
@@ -14,95 +14,237 @@ from flask import request, Flask, jsonify
 from flask_cors import CORS
 
 class QueryClassification:
-	def __init__(self, conv_list, path):
+	"""
+	QueryClassification is a class that detects user intent.
+	Given the user message, it classifies the user intent as either a question, rejection, or acceptance.
+
+		Args:
+			params(dict): A dict of parameters.
+	"""
+	def __init__(self, params):
+		
+		self.params = params
+
+		#Setting up FLASK
 		self.app = Flask(__name__)
 		CORS(self.app)
 
-		self.ques_list = ["Accepted papers in related to", 
-						"Recommend session or workshop in related to", 
-						"When is the session or workshop", 
-						"Authors participating in the session or workshop", 
+		self.conv_list = [] #ONLY FOR TESTING PURPOSES
+
+		#Question list
+		self.ques_list = ["When is the session or workshop",
+						"What time is the session or workshop",
+						"Who will be participating in the session or workshop",
+						"What authors are in the session or workshop",
+						"Will this author be in the session or workshop",
+						"What session or workshop will this author be in",
 						"What is the session or workshop about",
-						"What sessions or workshops are in",
+						"What is the session or workshop on",
+						"Recommend a session or workshop related to",
+						"Recommend a session or workshop author is in and related to",
+						"What papers does the session or workshop cover,"
+						"What are some accepted papers in the session or workshop",
+						"Recommend a session related to authors works",
+						"What are some sessions related to author's works",
+						"What are some papers about in the session or workshop",
 						"Papers related to",
-						"Which authors published most relevant papers to",
+						"What are some papers about",
 						"Give me papers made by",
 						"Papers written by"]
-		self.conv_list = conv_list
-		self.path = path
+
+		#Rejection and acceptance keywords
+		self.other_intents = {
+			'reject': ["Something else", "Anything else", "Not this", "Another one"],
+			'acceptance': ["Give me more about", "Give me more like this paper"]
+		}
+
+		#Entites refers to what medium the user requests for (paper, session, workshop)
+		self.entities = [['paper',  'article'], ['session'], ['workshop'], ['session and workshop', 'workshop and session']]
+
+		#List of all main conferences the chatbot can support
+		self.conference_list = ['SIGIR']
+
 		self.model = SentenceTransformer('multi-qa-mpnet-base-dot-v1')
-		self.entities = [['paper',  'article'], ['session'], ['workshop']]
+		self.tagger = SequenceTagger.load("flair/ner-english-large")
 
 		self.dense_index = DenseRetriever(self.model)
-		if os.path.exists('{}/index.pkl'.format(self.path)):
+		if os.path.exists('{}/index.pkl'.format(params['pickle_path'])):
 			print("true") #testing purposes
-			self.dense_index.load_index('{}/index.pkl'.format(self.path))
+			self.dense_index.load_index('{}/index.pkl'.format(params['pickle_path']))
 		else:
 			print("false") #testing purposes
 			self.dense_index.create_index_from_documents(self.ques_list)
-			self.dense_index.save_index(index_path='{}/index.pkl'.format(self.path), vectors_path='{}/vectors.pkl'.format(self.path))
+			self.dense_index.save_index(index_path='{}/index.pkl'.format(params['pickle_path']), vectors_path='{}/vectors.pkl'.format(params['pickle_path']))
 		
 		self.query_vector = None
-
-		self.tagger = SequenceTagger.load("flair/ner-english-large")
 	
+	#Testing purposes
 	def serve(self, port=80):
 		self.build_endpoints()
 		self.app.run(host='0.0.0.0', port=port)
 	
+	#Testing purposes
 	def build_endpoints(self):
 		@self.app.route('/encode', methods=['POST', 'GET'])
 		def encode_endpoint():
 			text = str(request.args.get('text'))
 			self.conv_list.insert(0, str(text))
-			da = self.create_DA()
-			temp = self.ques_list[da.intent]
+			da = self.create_DA(self.conv_list)
+			bestq = ''
+			if da.intent == 'rejection':
+				bestq = self.other_intents['reject'][da.intent_index]
+			elif da.intent == 'acceptance':
+				bestq = self.other_intents['acceptance'][da.intent_index]
+			else:
+				bestq = self.ques_list[da.intent_index]
+			temp = "intent: " + da.intent + ", best question: " + bestq
 			results = json.dumps(temp, indent=4)
 			return results
 	
-	def vectorize(self, batch_size=1):
+	#Tetsing purposes only
+	def add_message(self, msg):
+		self.conv_list.insert(0, msg)
+	
+	def vectorize(self, conv_list, batch_size=1):
+		"""
+		Vectorizes the most recent user query.
+
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+		
+		Returns:
+			A vector produced from the class' SentenceTransformers model on the user query.
+		"""
 		self.query_vector = self.model.encode(conv_list[0], batch_size)
 	
-	def sim_ques(self, query_vector):
-		dense_results = self.dense_index.search([query_vector], False)[0]
-		dense_results = [i[0] for i in dense_results]
+	def find_word(self, q, pattern):
+		"""
+		Checks if a given str contains specified pattern.
 
-		return dense_results[0]
+		Args:
+			q(str): Input string to search on.
+			pattern(str): Input string to look for in q.
+		
+		Returns:
+			A match object is q contains pattern, else None.
+		"""
+		return re.search(r'\b{0}\b'.format(pattern), q, flags=re.IGNORECASE)
+	
+	def check_other_intents(self, conv_list, intent): #'reject' or 'acceptance'
+		"""
+		Helper function to checks if usery query has intent of non-question type.
 
-	def main_conference(self, text_msg):
-		text = text_msg.lower()
-		if 'sigir' in text:
-			return 'sigir'
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+			intent(str): Either 'reject' or 'acceptance'. The specified intent to look for in user query.
+		
+		Returns:
+			intent if find_word() is not None, else None.
+		"""
+		for pattern in self.other_intents[intent]:
+			if self.find_word(conv_list[0], pattern) is not None:
+				return intent
 		return None
 	
-	def entity_keywords(self, text_msg):
-		text = text_msg.lower()
+	
+	def chack_main_intent(self, conv_list, batch_size=1):
+		"""
+		Checks user intent.
+
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+		
+		Returns:
+			A dict containing the intent ('reject', 'acceptance', 'question'), and the intent index (default=-1 if intent is non-question
+			type, for question intent it is the index of the most similar question).
+		"""
+		if len(conv_list) > 0:
+			if self.check_other_intents(conv_list, 'reject') is not None:
+				return {'intent': 'reject', 'intent_index': -1}
+			if self.check_other_intents(conv_list, 'acceptance') is not None:
+				return {'intent': 'acceptance', 'intent_index': -1}
+		
+		dense_results = self.dense_index.search([conv_list[0]])[0]
+		dense_results = [i[0] for i in dense_results]
+		return {'intent': 'question', 'intent_index': dense_results[0]}
+
+	def main_conference(self, conv_list):
+		"""
+		Checks if user referred to a conference.
+
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+		
+		Returns:
+			A str of the mentioned conference, else '' if user did not mention a conference.
+		"""
+		for conference in self.conference_list:
+			if self.find_word(conv_list[0], conference) is not None:
+				return conference
+		return ''
+	
+	def entity_keywords(self, conv_list):
+		"""
+		Checks if user referred to an entity.
+
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+		
+		Returns:
+			A str of the mentioned entity, else '' if user did not mention an entity.
+		"""
 		for entity in self.entities:
 			for word in entity:
-				if word in text:
+				if self.find_word(conv_list[0], word) is not None:
 					return word
-		return 'ERROR'
+		return ''
 	
-	def get_authors(self, text_msg):
+	def get_authors(self, conv_list):
+		"""
+		Checks if user referred to an author(s).
+
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+		
+		Returns:
+			A str list containing authors that the user mentioned, if any.
+		"""
 		authors = []
-		sentence = Sentence(text_msg)
+		sentence = Sentence(conv_list[0])
 		self.tagger.predict(sentence)
 		for entity in sentence.get_spans('ner'):
 			if entity.get_label("ner").value == "PER":
 				authors.append(entity.text)
 		return authors
 	
-	def create_DA(self):
-		self.vectorize()
-		intent = self.sim_ques(self.query_vector)
-		conference = self.main_conference(self.conv_list[0])
-		entity = self.entity_keywords(self.conv_list[0])
-		authors = self.get_authors(self.conv_list[0])
-		return DialogueAct(intent, conference, entity, self.query_vector, authors)
+	def create_DA(self, conv_list):
+		"""
+		Creates a DialogueAct based on user query to contain all needed information for dispatchment. 
+
+		Args:
+			conv_list(list): List of interaction_handler.msg.Message, each corresponding to a conversational message from / to the
+            user. This list is in reverse order, meaning that the first elements is the last interaction made by user.
+		
+		Returns:
+			A DialogueAct.
+		"""
+		self.vectorize(conv_list)
+		intent_dict = self.chack_main_intent(conv_list)
+		conference = self.main_conference(self.conv_list)
+		entity = self.entity_keywords(self.conv_list)
+		authors = self.get_authors(self.conv_list)
+		return DialogueAct(intent_dict['intent'], intent_dict['intent_index'], self.query_vector, conference, entity, authors)
 	
-if __name__ == "__main__":
-	conv_list = ["Is Catherine Qi going to be at SIGIR?"]
-	q = QueryClassification(conv_list, "D:/ERSP/chatbot/input_handler")
+if __name__ == "__main__": #TESTING PURPOSES
+	#conv_list = ["Is Catherine Qi going to be at SIGIR?"]
+	params = {'pickle_path': 'D:/ERSP/chatbot/input_handler'}
+	q = QueryClassification(params)
 	#da = q.create_DA()
 	#print(da.intent)
 	#print(da.main_conference)
