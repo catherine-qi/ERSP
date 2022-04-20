bfd840659727d807f2535e34382f4318f9194c23
CATHERINE QI <32625745+catherine-qi@users.noreply.github.com>
2022-04-20 00:56:35 -0400

    Delete da.py

--- chatbot/interaction_handler/da.py
+++
@@ -1,8 +0,0 @@
-class DialogueAct:
-    def __init__(self, intent, intent_index, main_conference, entity, query_vector, authors):
-         self.da = {'intent': intent,
-                   'intent index': intent_index,
-                   'main conference': main_conference,
-                   'entity': entity,
-                   'query vector': query_vector,
-                   'authors': authors}
